a3bb64d188e0a455695b895cd60f8837c0367667
Sarthak Khillon <skhillon@users.noreply.github.com>
2018-11-15 16:21:13 -0800

    Update server.R

--- server.R
+++ server.R
@@ -1,7 +1,7 @@
 #### Data processing ####
 sortedSOC <- visas$SOC_NAME %>%
-  toupper() %>%
-  table() %>% sort(decreasing = T)
+    toupper() %>%
+    table() %>% sort(decreasing = T)
 most.common.soc <- names(sortedSOC)[1]
 
 most.common.soc <- tail(names(sort(table(visas$SOC_NAME))), 1)
@@ -13,40 +13,42 @@ max.year <- max(visas$YEAR)
 #### End Data Processing ####
 
 function(input, output, session) {
-  #### Interactive Geography Map #####
-  sample.map.filtered <- visas %>%
-    filter(CASE_STATUS == input$CASE_STATUS &
-             FULL_TIME_POSITION == input$FULL_TIME_POSITION &
-             (YEAR == input$YEAR) &
-             PREVAILING_WAGE > input$PREVAILING_WAGE &
-             grepl(input$SOC_NAME, SOC_NAME, ignore.case = TRUE))
+    #### Interactive Geography Map #####
+    #sample.map.filtered <-
 
-  output$geographicVis <- renderPlot({
-    leaflet(sample.map.filtered) %>%
-      addTiles() %>%
-      addMarkers(~lon, ~lat, clusterOptions = markerClusterOptions())
-  })
+    output$geographicVis <- renderPlot({
+        leaflet(visas %>%
+                    filter(CASE_STATUS == input$CASE_STATUS &
+                               FULL_TIME_POSITION == input$FULL_TIME_POSITION &
+                               YEAR >= min(input$YEAR) &
+                               YEAR <= max(input$YEAR) &
+                               PREVAILING_WAGE >= min(input$PREVAILING_WAGE) &
+                               PREVAILING_WAGE <= max(input$PREVAILING_WAGE) &
+                               grepl(input$SOC_NAME, SOC_NAME, ignore.case = TRUE))) %>%
+            addTiles() %>%
+            addMarkers(~lon, ~lat, clusterOptions = markerClusterOptions())
+    })
 
-  #### Pie Chart ####
-  output$wageVis <- renderPlot({
-    visas %>%
-      filter(YEAR == input$YEAR) %>%
-      ggplot(aes(x = "", y = input$CASE_STATUS, fill = input$CASE_STATUS)) +
-      geom_bar(width = 1, stat = "identity") +
-      coord_polar("y", start = 0) +
-      xlab(NULL) +
-      ylab(NULL) +
-      theme(axis.text = element_blank(),
-            axis.ticks = element_blank(),
-            panel.grid = element_blank())
-  })
+    #### Pie Chart ####
+    output$wageVis <- renderPlot({
+        visas %>%
+            filter(YEAR == input$YEAR) %>%
+            ggplot(aes(x = "", y = input$CASE_STATUS, fill = input$CASE_STATUS)) +
+            geom_bar(width = 1, stat = "identity") +
+            coord_polar("y", start = 0) +
+            xlab(NULL) +
+            ylab(NULL) +
+            theme(axis.text = element_blank(),
+                  axis.ticks = element_blank(),
+                  panel.grid = element_blank())
+    })
 
-  output$acceptVis <- renderPlot({
-    visas %>%
-      filter(bewteen(visas$YEAR, input$YEAR, input$YEAR)) %>%
-      filter(between(visas$PREVAILING_WAGE, input$wage1, input$wage2)) %>%
-      ggplot(aes(x = input$var1, y = visas$PREVAILING_WAGE)) +
-      geom_smooth(method = "lm")
-  })
-  session$onSessionEnded(stopApp)
+    output$acceptVis <- renderPlot({
+        visas %>%
+            filter(bewteen(visas$YEAR, input$YEAR, input$YEAR)) %>%
+            filter(between(visas$PREVAILING_WAGE, input$wage1, input$wage2)) %>%
+            ggplot(aes(x = input$var1, y = visas$PREVAILING_WAGE)) +
+            geom_smooth(method = "lm")
+    })
+    session$onSessionEnded(stopApp)
 }
