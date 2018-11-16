50c90d1d9e338ec535466002e57d5b7fcc6a87b4
Sarthak Khillon <sarthakkhillon24@gmail.com>
2018-11-15 16:12:07 -0800

    Moved imports to global.R, made sure output matched what was expected in ui.R

--- server.R
+++ server.R
@@ -1,50 +1,35 @@
-library(tidyverse)
-library(readr)
-library(stringr)
-library(leaflet)
-library(ggplot2)
-library(shiny)
-
 #### Data processing ####
-
-visas <- read_csv("visas.csv")
 sortedSOC <- visas$SOC_NAME %>%
   toupper() %>%
   table() %>% sort(decreasing = T)
-most.common.soc <- names(sortedSOC)[1] 
+most.common.soc <- names(sortedSOC)[1]
 
-#raw.input <- read_csv("h1b_kaggle.csv")
+most.common.soc <- tail(names(sort(table(visas$SOC_NAME))), 1)
+#most.common.soc
 
-h1b.apps <- read_csv("visas.csv") #raw.input %>%
-#na.omit() %>%
-#sample_n(10000)
-
-most.common.soc <- tail(names(sort(table(h1b.apps$SOC_NAME))), 1)
-most.common.soc 
-
-min.year <- min(h1b.apps$YEAR)
-max.year <- max(h1b.apps$YEAR)
+min.year <- min(visas$YEAR)
+max.year <- max(visas$YEAR)
 
 #### End Data Processing ####
 
 function(input, output, session) {
   #### Interactive Geography Map #####
-  sample.map.filtered <- h1b.apps %>%
+  sample.map.filtered <- visas %>%
     filter(CASE_STATUS == input$CASE_STATUS &
-             FULL_TIME_POSITION == input$FULL_TIME_POSITION & 
-             (YEAR == input$YEAR) & 
-             PREVAILING_WAGE > input$PREVAILING_WAGE & 
+             FULL_TIME_POSITION == input$FULL_TIME_POSITION &
+             (YEAR == input$YEAR) &
+             PREVAILING_WAGE > input$PREVAILING_WAGE &
              grepl(input$SOC_NAME, SOC_NAME, ignore.case = TRUE))
-  
-  output$plot1 <- renderPlot({
-    leaflet(sample.map.filtered) %>% 
+
+  output$geographicVis <- renderPlot({
+    leaflet(sample.map.filtered) %>%
       addTiles() %>%
       addMarkers(~lon, ~lat, clusterOptions = markerClusterOptions())
   })
-  
+
   #### Pie Chart ####
-  output$plot2 <- renderPlot({
-    h1b.apps %>%
+  output$wageVis <- renderPlot({
+    visas %>%
       filter(YEAR == input$YEAR) %>%
       ggplot(aes(x = "", y = input$CASE_STATUS, fill = input$CASE_STATUS)) +
       geom_bar(width = 1, stat = "identity") +
@@ -55,12 +40,12 @@ function(input, output, session) {
             axis.ticks = element_blank(),
             panel.grid = element_blank())
   })
-  
-  output$plot3 <- renderPlot({
-    h1b.apps %>%
-      filter(bewteen(h1b.apps$YEAR, input$YEAR, input$YEAR)) %>%
-      filter(between(h1b.apps$PREVAILING_WAGE, input$wage1, input$wage2)) %>%
-      ggplot(aes(x = input$var1, y = h1b.apps$PREVAILING_WAGE)) + 
+
+  output$acceptVis <- renderPlot({
+    visas %>%
+      filter(bewteen(visas$YEAR, input$YEAR, input$YEAR)) %>%
+      filter(between(visas$PREVAILING_WAGE, input$wage1, input$wage2)) %>%
+      ggplot(aes(x = input$var1, y = visas$PREVAILING_WAGE)) +
       geom_smooth(method = "lm")
   })
   session$onSessionEnded(stopApp)
