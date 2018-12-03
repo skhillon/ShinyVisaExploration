2e4badffd49ad749f928bc6901183a3a9acf5a99
Sarthak Khillon <sarthakkhillon24@gmail.com>
2018-12-02 20:23:01 -0800

    Tidy names

--- global.R
+++ global.R
@@ -10,6 +10,14 @@ library(leaflet)
 library(ggplot2)
 library(shiny)
 library(RColorBrewer)
+library(tools)
 
 load("visa_info.RData")
+
+# Convert all SOC, and Job Title fields to Title Case.
+# We leave Employers as all-caps because Company names vary greatly.
+visas$SOC_NAME <- toTitleCase(tolower(visas$SOC_NAME))
+visas$JOB_TITLE <- toTitleCase(tolower(visas$JOB_TITLE))
+
+# Set color palette.
 statusPalette <- brewer.pal(9, "Set1")
