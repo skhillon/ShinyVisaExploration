c2f3a16bd1a095811599bad5c91b4ee75e77e3a7
Sarthak Khillon <sarthakkhillon24@gmail.com>
2018-12-02 17:56:52 -0800

    Issue 9 and put it back to Normal mode

--- ui.R
+++ ui.R
@@ -11,8 +11,8 @@ shinyUI(fluidPage(theme = shinytheme("paper"),
    # If none are selected, then All will toggle every radio element to ON.
    tabsetPanel(
       type = "tabs",
-      tabPanel("Interactive Map", 
-               sidebarLayout(        
+      tabPanel("Interactive Map",
+               sidebarLayout(
                   sidebarPanel(
                   # CASE_STATUS only has the following 4 values:
                   #- Certified-Withdrawn
@@ -21,7 +21,7 @@ shinyUI(fluidPage(theme = shinytheme("paper"),
                   #- Denied
                   # As such, this will be a radio element.
                   checkboxGroupInput(
-                     "CASE_STATUS_MAP", "Case Status",     
+                     "CASE_STATUS_MAP", "Case Status",
                      choices = list(
                         "Certified-Withdrawn" = "CERTIFIED-WITHDRAWN",
                         "Certified" = "CERTIFIED",
@@ -31,35 +31,44 @@ shinyUI(fluidPage(theme = shinytheme("paper"),
                      selected = c("CERTIFIED-WITHDRAWN", "CERTIFIED", "WITHDRAWN", "DENIED")
                   ),
                   hr(),
-                  
+
                   # There are many values for EMPLOYER_NAME, so this will be an autocomplete text field.
                   selectizeInput("EMPLOYER_NAME_MAP", "Employer Name",
                                  choices = visas$EMPLOYER_NAME,
-                                 options = list(maxItems = 1)),
+                                 options = list(
+                                     placeholder = "Try \"APPLE INC.\"",
+                                     maxItems = 1),
+                                 selected = "APPLE INC."),
                   br(),
-                  
+
                   # Similarly, there are also many values for SOC_NAME. This will also be a text field.
                   selectizeInput("SOC_NAME_MAP", "SOC Name",
                                  choices = visas$SOC_NAME,
-                                 options = list(maxItems = 1)),
+                                 options = list(
+                                     placeholder = "Try \"Software Developers, Applications\"",
+                                     maxItems = 1),
+                                 selected = "Software Developers, Applications"),
                   br(),
-                  
+
                   # Another text field for JOB_TITLE
                   selectizeInput("JOB_TITLE_MAP", "Job Title",
                                  choices = visas$JOB_TITLE,
-                                 options = list(maxItems = 1)),
+                                 options = list(
+                                     placeholder = "Try \"SOFTWARE ENGINEER\"",
+                                     maxItems = 1),
+                                 selected = "SOFTWARE ENGINEER"),
                   br(),
-                  
+
                   # Full Time Position is either a yes or a no, so this is a 2-button radio section.
                   radioButtons("FULL_TIME_POSITION_MAP", "Job Type",
                                c("All", "Full Time", "Part Time")),
                   br(),
-                  
+
                   # PREVAILING_WAGE will be an inclusive slider.
                   sliderInput("PREVAILING_WAGE_MAP", "Wage Range", min = MIN.WAGE,
                               max = MAX.WAGE, value = c(MIN.WAGE, MAX.WAGE)),
                   br(),
-                  
+
                   # There are very few years that are unique in the dataset, so this will be an inclusive slider.
                   sliderInput("YEAR_MAP", "Year Range", min = MIN.YEAR,
                               max = MAX.YEAR, value = c(MIN.YEAR, MAX.YEAR), step = 1,
@@ -68,11 +77,11 @@ shinyUI(fluidPage(theme = shinytheme("paper"),
                   mainPanel(leafletOutput("geographicVis"))
                   )
                ),
-      
+
       #Pie chart
       tabPanel(
-         "Top Earners", 
-         sidebarLayout(        
+         "Top Earners",
+         sidebarLayout(
             sidebarPanel(
                # CASE_STATUS only has the following 4 values:
                #- Certified-Withdrawn
@@ -81,7 +90,7 @@ shinyUI(fluidPage(theme = shinytheme("paper"),
                #- Denied
                # As such, this will be a checkbox element.
                checkboxGroupInput(
-                  "CASE_STATUS_PIE", "Case Status",     
+                  "CASE_STATUS_PIE", "Case Status",
                   choices = list(
                      "Certified-Withdrawn" = "CERTIFIED-WITHDRAWN",
                      "Certified" = "CERTIFIED",
@@ -91,35 +100,35 @@ shinyUI(fluidPage(theme = shinytheme("paper"),
                      selected = c("CERTIFIED-WITHDRAWN", "CERTIFIED", "WITHDRAWN", "DENIED")
                   ),
                   hr(),
-                  
+
                   # There are many values for EMPLOYER_NAME, so this will be an autocomplete text field.
                   selectizeInput("EMPLOYER_NAME_PIE", "Employer Name",
                                  choices = visas$EMPLOYER_NAME,
                                  options = list(maxItems = 1)),
                   br(),
-                  
+
                   # Similarly, there are also many values for SOC_NAME. This will also be a text field.
                   selectizeInput("SOC_NAME_PIE", "SOC Name",
                                  choices = visas$SOC_NAME,
                                  options = list(maxItems = 1)),
                   br(),
-                  
+
                   # Another text field for JOB_TITLE
                   selectizeInput("JOB_TITLE_PIE", "Job Title",
                                  choices = visas$JOB_TITLE,
                                  options = list(maxItems = 1)),
                   br(),
-                  
+
                   # Full Time Position is either a yes or a no, so this is a 2-button radio section.
                   radioButtons("FULL_TIME_POSITION_PIE", "Job Type",
                                c("All", "Full Time", "Part Time")),
                   br(),
-                  
+
                   # PREVAILING_WAGE will be an inclusive slider.
                   sliderInput("PREVAILING_WAGE_PIE", "Wage Range", min = MIN.WAGE,
                               max = MAX.WAGE, value = c(MIN.WAGE, MAX.WAGE)),
                   br(),
-                  
+
                   # There are very few years that are unique in the dataset, so this will be an inclusive slider.
                   sliderInput("YEAR_PIE", "Year Range", min = MIN.YEAR,
                               max = MAX.YEAR, value = c(MIN.YEAR, MAX.YEAR), step = 1,
@@ -130,8 +139,8 @@ shinyUI(fluidPage(theme = shinytheme("paper"),
                   )
                )
          ),
-      tabPanel("Acceptance Rates", 
-               sidebarLayout(        
+      tabPanel("Acceptance Rates",
+               sidebarLayout(
                   sidebarPanel(
                   # CASE_STATUS only has the following 4 values:
                   #- Certified-Withdrawn
@@ -140,7 +149,7 @@ shinyUI(fluidPage(theme = shinytheme("paper"),
                   #- Denied
                   # As such, this will be a checkbox element.
                      checkboxGroupInput(
-                        "CASE_STATUS", "Case Status",     
+                        "CASE_STATUS", "Case Status",
                         choices = list(
                            "Certified-Withdrawn" = "CERTIFIED-WITHDRAWN",
                            "Certified" = "CERTIFIED",
@@ -150,35 +159,35 @@ shinyUI(fluidPage(theme = shinytheme("paper"),
                         selected = c("CERTIFIED-WITHDRAWN", "CERTIFIED", "WITHDRAWN", "DENIED")
                      ),
                      hr(),
-                  
+
                   # There are many values for EMPLOYER_NAME, so this will be an autocomplete text field.
                   selectizeInput("EMPLOYER_NAME", "Employer Name",
                                  choices = visas$EMPLOYER_NAME,
                                  options = list(maxItems = 1)),
                   br(),
-                  
+
                   # Similarly, there are also many values for SOC_NAME. This will also be a text field.
                   selectizeInput("SOC_NAME", "SOC Name",
                                  choices = visas$SOC_NAME,
                                  options = list(maxItems = 1)),
                   br(),
-                  
+
                   # Another text field for JOB_TITLE
                   selectizeInput("JOB_TITLE", "Job Title",
                                  choices = visas$JOB_TITLE,
                                  options = list(maxItems = 1)),
                   br(),
-                  
+
                   # Full Time Position is either a yes or a no, so this is a 2-button radio section.
                   radioButtons("FULL_TIME_POSITION", "Job Type",
                                c("All", "Full Time", "Part Time")),
                   br(),
-                  
+
                   # PREVAILING_WAGE will be an inclusive slider.
                   sliderInput("PREVAILING_WAGE", "Wage Range", min = MIN.WAGE,
                               max = MAX.WAGE, value = c(MIN.WAGE, MAX.WAGE)),
                   br(),
-                  
+
                   # There are very few years that are unique in the dataset, so this will be an inclusive slider.
                   sliderInput("YEAR", "Year Range", min = MIN.YEAR,
                               max = MAX.YEAR, value = c(MIN.YEAR, MAX.YEAR), step = 1,
@@ -190,57 +199,3 @@ shinyUI(fluidPage(theme = shinytheme("paper"),
       )
    )
 )
-#        sidebarLayout(
-#         sidebarPanel(
-#             # CASE_STATUS only has the following 4 values:
-#             #- Certified-Withdrawn
-#             #- Certified
-#             #- Withdrawn
-#             #- Denied
-#             # As such, this will be a radio element.
-#             radioButtons("CASE_STATUS", "Case Status",
-#                          c("All", "Certified-Withdrawn", "Certified", "Withdrawn", "Denied")),
-#             br(),
-# 
-#             # There are many values for EMPLOYER_NAME, so this will be an autocomplete text field.
-#             selectizeInput("EMPLOYER_NAME", "Employer Name",
-#                            choices = visas$EMPLOYER_NAME,
-#                            options = list(maxItems = 1)),
-#             br(),
-# 
-#             # Similarly, there are also many values for SOC_NAME. This will also be a text field.
-#             selectizeInput("SOC_NAME", "SOC Name",
-#                            choices = visas$SOC_NAME,
-#                            options = list(maxItems = 1)),
-#             br(),
-# 
-#             # Another text field for JOB_TITLE
-#             selectizeInput("JOB_TITLE", "Job Title",
-#                            choices = visas$JOB_TITLE,
-#                            options = list(maxItems = 1)),
-#             br(),
-# 
-#             # Full Time Position is either a yes or a no, so this is a 2-button radio section.
-#             radioButtons("FULL_TIME_POSITION", "Job Type",
-#                          c("All", "Full Time", "Part Time")),
-#             br(),
-# 
-#             # PREVAILING_WAGE will be an inclusive slider.
-#             sliderInput("PREVAILING_WAGE", "Wage Range", min = MIN.WAGE,
-#                         max = MAX.WAGE, value = c(MIN.WAGE, MAX.WAGE)),
-#             br(),
-# 
-#             # There are very few years that are unique in the dataset, so this will be an inclusive slider.
-#             sliderInput("YEAR", "Year Range", min = MIN.YEAR,
-#                         max = MAX.YEAR, value = c(MIN.YEAR, MAX.YEAR), step = 1,
-#                         ticks = T, sep = "")
-#         ),
-# 
-#         mainPanel(
-#             tabsetPanel(type = "tabs",
-#                         tabPanel("Interactive Map", leafletOutput("geographicVis")),
-#                         tabPanel("Top Earners", plotOutput("wageVis")),
-#                         tabPanel("Acceptance Rates", plotOutput("acceptVis")))
-#         )
-#     )
-# ))
