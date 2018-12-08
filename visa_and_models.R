f61bf941c9756d766a015e5bdca72e707ca3930e
Sarthak Khillon <sarthakkhillon24@gmail.com>
2018-12-08 12:56:22 -0800

    Added classifier to R script

--- visa_and_models.R
+++ visa_and_models.R
@@ -1,6 +1,9 @@
+library(tidyverse)  # Misc tidy data wrapper package.
 library(tools)  # Only used for toTitleCase()
-library(class)  # KNN
+library(caret)  # KNN model
+library(ggplot2)  # Plotting
 
+# 1 ===== DATA CLEANING =====
 load("visa_info.RData")
 
 # Convert all SOC, and Job Title fields to Title Case.
@@ -8,15 +11,50 @@ load("visa_info.RData")
 visas$SOC_NAME <- toTitleCase(tolower(visas$SOC_NAME))
 visas$JOB_TITLE <- toTitleCase(tolower(visas$JOB_TITLE))
 
+# 2 ===== CASE STATUS CLASSIFIER =====
+
 # KNN model to predict categorical Case Status given user input.
 # Since KNNs do not continuously "learn" and dimensionality is low, it is a useful choice.
-# Source: https://dzone.com/refcardz/machine-learning-predictive?chapter=9
-train_input <- as.matrix(iristrain[,-5])
-train_output <- as.vector(iristrain[,5])
+# Source: http://dataaspirant.com/2017/01/09/knn-implementation-r-using-caret-package/
 
-predict_case <- function(user_info) {
-    knn(train_input, user_info, train_output, k = 5)
+# Functions
+encode_numeric <- function(df) {
+    df$emp_encoding <- as.numeric(as.factor(df$EMPLOYER_NAME))
+    df$soc_encoding <- as.numeric(as.factor(df$SOC_NAME))
+    df$job_title_encoding <- as.numeric(as.factor(df$JOB_TITLE))
+    df$ft_encoding <- as.numeric(as.factor(df$FULL_TIME_POSITION))
+    df$worksite_encoding <- as.numeric(as.factor(df$WORKSITE))
+    df$year_encoding <- as.numeric(df$YEAR)
+
+    return(df)
 }
 
-# Linear regression model for predicted wage.
+# Setup
+num_cases <- length(unique(visas$CASE_STATUS))
+set.seed(37)
+visa_colnames <- colnames(visas)[3:11]
+cluster_eval_cols <- setdiff(visa_colnames, "PREVAILING_WAGE")
+visas <- encode_numeric(visas)
+
+# Train-test split
+in.train.rows <- createDataPartition(visas$CASE_STATUS, p = 0.7, list = FALSE)
+in.train.cols <- union(2, 10:17)
+v.train <- visas[in.train.rows, in.train.cols]
+v.test <- visas[-in.train.rows, in.train.cols]
+v.train$CASE_STATUS <- factor(v.train$CASE_STATUS)
+
+# Build classifier
+train_control <- trainControl(method = "repeatedcv", number = 10, repeats = 3)
+classifier <- train(CASE_STATUS ~., data = v.train, method = "knn",
+                    trControl = train_control,
+                    preProcess = c("center", "scale"),
+                    tuneLength = 10)
+
+# External Interface.
+predict_case <- function(user_df) {
+    user_numeric_df <- encode_numeric(user_df)
+    predict(classifier, newdata = user_numeric_df)
+}
+
+# 3 ===== PREDICTED WAGE REGRESSOR =====
 wage_model <- lm()
